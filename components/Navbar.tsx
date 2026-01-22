8d8bb6590105d5f3b56f931a8edf213e774a6ee1
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2026-01-22 15:45:28 +0530

    Implement Navbar component with avatar and dashboard link; add Avatar UI components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Moon } from "lucide-react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="p-4 flex items-center justify-between sticky top-0 bg-background z-10">
+      {/* LEFT */}
+      CollapseButton
+      {/* RIGHT */}
+      <div>
+        <Link href="/">Dashboard</Link>
+        <Moon />
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>HA</AvatarFallback>
+        </Avatar>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
